9a4875a49deca198d93e3f857bea40bec08909ab
Vincent Lonij <29819815+vincentropy@users.noreply.github.com>
2026-06-02 17:41:42 +0200

    Update example data and add test that uses example data.

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,8 +6,10 @@ from spextract import models
 class TestModels(unittest.TestCase):
     def test_parser_spec_model_reads_example_spec(self):
         spec = models.ParseSpec.from_yaml_file("tests/example_data/example_spec.yaml")
-        self.assertEqual(spec.name, "example_spec_name")
-        self.assertIn("title", spec.fields)
-        self.assertIn("links", spec.fields)
-        self.assertEqual(spec.fields["title"].type, models.FieldType.TEXT)
-        self.assertEqual(spec.fields["links"].type, models.FieldType.LINK)
+        self.assertEqual(spec.name, "example_spec")
+        self.assertIn("page_title", spec.fields)
+        self.assertIn("cities", spec.fields)
+        self.assertEqual(spec.fields["page_title"].type, models.FieldType.TEXT)
+        self.assertEqual(spec.fields["cities"].multiple, True)
+        self.assertEqual(spec.fields["cities"].fields["name"].type, models.FieldType.TEXT)
+        self.assertEqual(spec.fields["cities"].fields["url"].type, models.FieldType.LINK)
